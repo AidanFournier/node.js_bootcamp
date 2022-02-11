060beb8be9133334046493de2a12355963c260d8
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-02-11 23:50:50 +0900

    Fixed issue with finding modules

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+// fs stands for file system
+// by using this module, we will get access to functions for reading and writing data
+// this module will return an object for which there are lots of functions we can used, stored in the fs variable
+
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8"); // Sync stands for synchronous (vs asynchronous)
+// this takes two arguments: first is the path to th file we're reading, second we deefine the character encoding
+// calling this function will rread the data we're giving to it and return it to us, and we need to save this into a variable
+console.log(textIn);
