d7596d80d4c1b4a152f9fa661d702efad3b4802f
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-04-20 16:10:51 +0900

    Learning about caching

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -10,3 +10,11 @@ console.log(calc1.add(2, 5));
 // const calc2 = require('./test-module-2');
 const { add, multiply, divide } = require('./test-module-2'); // we can use ES6 destucturing; these names have to be same as property names on object
 console.log(multiply(2, 5));
+
+//caching
+// we have caching in node.js modules
+require("./test-module-3")();  // we could put this in a variable. but we dont have to
+require("./test-module-3")();
+require("./test-module-3")();
+
+// technically, this module was loaded only once because of caching, so the top level code (Hello...) was only logged once at the beginning
---
+++ 2-how-node-works/test-module-3.js
@@ -0,0 +1,3 @@
+console.log('Hello from the module') // some top-level code
+
+module.exports = () => console.log('Log this beautiful text');
