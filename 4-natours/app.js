a01d13203c76212f23717c83e2b69ab1435be83b
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-04-29 21:02:10 +0900

    Handled post requests for api

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -3,6 +3,11 @@ const express = require('express');
 
 const app = express();
 
+// this is middleware, which is a function that can modify the incming request data
+// called middleware because it stands between the request and the response
+// in the case of our post request, it's being used so that the data from the body is added to the request object
+app.use(express.json());
+
 // this is called the route handler:
 // app.get('/', (req, res) => {
 //     res.status(200)
@@ -26,6 +31,26 @@ app.get('/api/v1/tours', (req, res) => {
         }
     })
 })
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    // usually, database taakes care of adding id to new object; since we're working with a file as our 'database' rn, we need to set the id
+    const newId = tours[tours.length -1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body); // this combines two objects
+
+    tours.push(newTour);
+    
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours), 
+        err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour
+                }
+            });
+    });
+});
 
 const port = 3000;
 app.listen(port, () => {
