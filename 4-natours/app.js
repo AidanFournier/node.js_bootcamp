fffb0fcd3f48c1d830d52cd4457a7f3671e2ea08
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-05-04 19:42:15 +0900

    Mounted routers for tours and users

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -159,28 +159,33 @@ const deleteUser = (req, res) => {
 // app.delete('/api/v1/tours/:id', deleteTour);
 
 const tourRouter = express.Router();
+const userRouter = express.Router();
+// this tourRouter is actually middleware
 
-app
-    .route('/api/v1/tours')
+tourRouter
+    .route('/')
     .get(getAllTours)
     .post(createTour);
 
-app
-    .route('/api/v1/tours/:id')
+tourRouter
+    .route('/:id')
     .get(getTour)
     .patch(updateTour)
     .delete(deleteTour);
 
-app
-    .route('/api/v1/users')
+userRouter
+    .route('/')
     .get(getAllUsers)
     .post(createUser);
 
-app
-    .route('/api/v1/users/:id')
+userRouter
+    .route('/:id')
     .get(getUser)
     .patch(updateUser)
     .delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', tourRouter);
 
 // Start server:
 const port = 3000;
