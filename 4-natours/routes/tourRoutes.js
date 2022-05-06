85be295e50a695c752940786b9ec582bab9e4685
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-05-06 23:49:55 +0900

    Created param middleware

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -4,6 +4,11 @@ const tourController = require('./../controllers/tourController');
 // Routes:
 const router = express.Router();
 
+router.param('id', (req, res, next, val) => {
+    console.log(`Tour id is: ${id}`);
+    next();
+})
+
 router
     .route('/')
     .get(tourController.getAllTours)
