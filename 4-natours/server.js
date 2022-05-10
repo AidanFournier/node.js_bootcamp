70a73dcd889e9873b4cb7cecf1e14e2684ae9453
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-05-10 22:42:40 +0900

    Added environment variables

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' }); // this command reads our variables from the file & saves them into Node.js environment variables
+
 const app = require('./app');
 
 // Start server:
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on ${port}`);
 });
