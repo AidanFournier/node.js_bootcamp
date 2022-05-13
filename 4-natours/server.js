56d8047dda9ac966aa75a8853b1f06ca55e436fb
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-05-13 22:58:48 +0900

    Added eslint and prettier packages, configured

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' }); // this command reads our variables from the file & saves them into Node.js environment variables
-
 const app = require('./app');
+
+dotenv.config({ path: './config.env' }); // this command reads our variables from the file & saves them into Node.js environment variables
 
 // Start server:
 const port = process.env.PORT || 3000;
