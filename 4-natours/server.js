4db1bdda965436eb869082386546d19aca99774c
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-06-28 22:56:21 +0900

    Added mongoose and connected express to the cloud database

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' }); // this command reads our variables from the file & saves them into Node.js environment variables
+
+// We configure MongoDB in this serrver file:
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+.then(() => console.log('DB connection successful'));
+
 
 // Start server:
 const port = process.env.PORT || 3000;
