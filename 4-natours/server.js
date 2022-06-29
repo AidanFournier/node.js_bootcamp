fc5ee9d14f6c48a6d5cd4fc7bad4fc0208845da1
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-06-29 22:15:31 +0900

    Created tour model using mongoose

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -18,6 +18,22 @@ mongoose
 })
 .then(() => console.log('DB connection successful'));
 
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 // Start server:
 const port = process.env.PORT || 3000;
